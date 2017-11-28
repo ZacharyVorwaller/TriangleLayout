03d5b4820226b3391d3b65d908d8c3ab018c1274
Zachary Vorwaller <Zachary Vorwaller>
2017-11-28 10:19:38 -0700

    Add project files.

---
+++ TriangleLayout/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TriangleLayout._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <asp:Label ID="LabelV1x" runat="server" Text="V1x:"></asp:Label>
+        <asp:TextBox ID="TextBoxV1x" runat="server"></asp:TextBox>
+        <asp:Label ID="LabelV1y" runat="server" Text="V1y:"></asp:Label>
+        <asp:TextBox ID="TextBoxV1y" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="LabelV2x" runat="server" Text="V2x:"></asp:Label>
+        <asp:TextBox ID="TextBoxV2x" runat="server"></asp:TextBox>
+        <asp:Label ID="LabelV2y" runat="server" Text="V2y:"></asp:Label>
+        <asp:TextBox ID="TextBoxV2y" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="LabelV3x" runat="server" Text="V3x:"></asp:Label>
+        <asp:TextBox ID="TextBoxV3x" runat="server"></asp:TextBox>
+        <asp:Label ID="LabelV3y" runat="server" Text="V3y:"></asp:Label>
+        <asp:TextBox ID="TextBoxV3y" runat="server"></asp:TextBox>
+    &nbsp;<asp:Button ID="ButtonSolve" runat="server" Text="Solve" OnClick="ButtonSolve_Click" />
+    </div>
+    <div id="Results" runat="server"></div>
+</asp:Content>
---
+++ TriangleLayout/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TriangleLayout
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSolve_Click(object sender, EventArgs e)
+        {
+            string result = String.Empty;
+            int x1 = -1, x2 = -1, x3 = -1, y1 = -1, y2 = -1, y3 = -1;
+            List<int> xcords = new List<int>();
+            List<int> ycords = new List<int>();
+
+            if (Int32.TryParse(TextBoxV1x.Text, out x1) && Int32.TryParse(TextBoxV2x.Text, out x2) && Int32.TryParse(TextBoxV3x.Text, out x3)
+                && Int32.TryParse(TextBoxV1y.Text, out y1) && Int32.TryParse(TextBoxV2y.Text, out y2) && Int32.TryParse(TextBoxV3y.Text, out y3))
+            {
+                if (((xcords.Max() - xcords.Min()) == 10) && ((ycords.Max() - ycords.Min()) == 10))
+                {
+                    xcords.Add(x1);
+                    xcords.Add(x2);
+                    xcords.Add(x3);
+                    ycords.Add(y1);
+                    ycords.Add(y2);
+                    ycords.Add(y3);
+
+
+                    result = FindTriangle(xcords, ycords);
+                }
+                else
+                {
+                    result = "Please enter coordinates for just one triangle";
+                }
+            }
+            else
+            {
+                result = "Please enter a vaild number for each coordinate";
+            }
+            Results.InnerHtml = result;
+        }
+
+        public string FindTriangle(List<int> xcords, List<int> ycords)
+        {
+            xcords.Sort();
+            ycords.Sort();
+
+            var xMedianValue = xcords[1];
+            var yMedianValue = ycords[1];
+            var xMaxValue = xcords.Max();
+            var yMaxValue = ycords.Max();
+
+            string row = IntToLetters(yMaxValue / 10);
+            string column = ((xMedianValue / 10) + (xMaxValue / 10)).ToString();
+
+            return row + column;
+        }
+        public static string IntToLetters(int value)
+        {
+            string result = string.Empty;
+            while (--value >= 0)
+            {
+                result = (char)('A' + value % 26) + result;
+                value /= 26;
+            }
+            return result;
+        }
+    }
+}
